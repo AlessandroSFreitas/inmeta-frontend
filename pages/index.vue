c3c4a06f9e8372df1f04d05fad6ec9a5694f6ef1
Alessandro Freitas <alessandro_freitas@icloud.com>
2025-04-01 15:13:12 -0300

    fix url

--- pages/index.vue
+++ pages/index.vue
@@ -44,7 +44,7 @@ const config = useRuntimeConfig();
 
 const handleLogin = async () => {
   try {
-    const response = await fetch('https://backend-case-k15vpydzi-alessandrosfreitas-projects.vercel.app/api/auth/login', {
+    const response = await fetch('https://backend-case-xi.vercel.app/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: username.value, password: password.value }),
